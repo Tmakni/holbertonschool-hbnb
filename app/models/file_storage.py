372ef257f4436ff5b194941440b50023b66c3316
tom <tom.makni@gmail.com>
2025-03-12 09:24:09 +0100

    file_storage push

--- app/models/file_storage.py
+++ app/models/file_storage.py
@@ -1,7 +1,14 @@
 import json
+"""
+Import JSON
+"""
 
 
 class FileStorage:
+    """
+   Class file storages, file_path chemin ou enregistre Obj,
+   __objects dictionnaire qui contien les objects dans la mémoire
+    """
     __file_path = "storage.json"
     __objects = {}
 
@@ -12,6 +19,25 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-
+        """
+        Ajout de nouveau OBJ a l'enregistrement
+        """
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
+
+    def save(self):
+        """
+        Convertit tous les objets en dictionnaires
+        """
+        with open(self.__file_path, "w") as f:
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
+
+    def reload(self):
+        """
+        Ouvre storage.json et charge les données
+        """
+        try:
+            with open(self.__file_path, "r") as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            pass
