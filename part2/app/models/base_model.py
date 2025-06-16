c86fa178f0b5da3ea67397b1fa6f553451eb2d0f
Tmakni <tom.makni@gmail.com>
2025-06-16 14:40:17 +0200

    base_model add

---
+++ part2/app/models/base_model.py
@@ -0,0 +1,13 @@
+import uuid
+from datetime import datetime
+
+def __init__(self, **kwargs):
+    self.id = str(uuid.uuid4())
+
+    now = datetime.utcnow()
+    self.created_at = now
+    self.updated_at = now
+
+    for key, value in kwargs.items():
+        if hasattr(self, key):
+            setattr(self, key, value)
