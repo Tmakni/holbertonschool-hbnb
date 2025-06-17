39047459c30a993b93484b15e2b3101c36750625
Tmakni <tom.makni@gmail.com>
2025-06-17 15:23:19 +0200

    add amenities basemodel place review user

--- part2/app/models/base_model.py
+++ part2/app/models/base_model.py
@@ -1,15 +1,19 @@
-#!/usr/bin/python3
 import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self, **kwargs):
+    def __init__(self):
         self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-        now = datetime.utcnow()
-        self.created_at = now
-        self.updated_at = now
+    def save(self):
+        """Update the updated_at timestamp whenever the object is modified"""
+        self.updated_at = datetime.now()
 
-        for key, value in kwargs.items():
+    def update(self, data):
+        """Update the attributes of the object based on the provided dictionary"""
+        for key, value in data.items():
             if hasattr(self, key):
                 setattr(self, key, value)
+        self.save()
--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-class User (BaseModel):
 
-    def __init__(self, first_name: str, last_name: str, email: str, **kwargs):
-        if not first_name:
-            raise TypeError("Add first name")
-        if not last_name:
-            raise TypeError("Add last name")
-        if not email:
-            raise TypeError("Add a email")
-        if email and "@" not in email:
-            raise ValueError("format invalid")
 
-        self.first_name = first_name
-        self.last_ame = last_name
-        self.email = email
-        super().__init__(**kwargs)
+from flask_restx import Namespace, Resource, fields
+from app.services import facade
+
+api = Namespace('users', description='User operations')
+user_model = api.model('User', {
+    'first_name': fields.String(required=True, description='First name of the user'),
+    'last_name': fields.String(required=True, description='Last name of the user'),
+    'email': fields.String(required=True, description='Email of the user')
+})
+
+@api.route('/')
+class UserList(Resource):
+    @api.expect(user_model, validate=True)
+    @api.response(201, 'User successfully created')
+    @api.response(400, 'Email already registered')
+    @api.response(400, 'Invalid input data')
+    def post(self):
+        """Register a new user"""
+        user_data = api.payload
+
+        existing_user = facade.get_user_by_email(user_data['email'])
+        if existing_user:
+            return {'error': 'Email already registered'}, 400
+
+        new_user = facade.create_user(user_data)
+        return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
