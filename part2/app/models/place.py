9358d1ee65bb6ab247785e530a504cf0d39a1de2
julien <julienvillessot@gmail.com>
2025-06-18 13:18:21 +0200

    correction user, place

--- part2/app/models/place.py
+++ part2/app/models/place.py
@@ -1,59 +1,30 @@
 #!/usr/bin/python3
 
 
-from flask_restx import Namespace, Resource, fields
-from app.services import facade
+from .basemodel import BaseModel
 
-api = Namespace('places', description='Place operations')
 
-amenity_model = api.model('PlaceAmenity', {
-    'id': fields.String(description='Amenity ID'),
-    'name': fields.String(description='Name of the amenity')
-})
+class Place(BaseModel):
 
-user_model = api.model('PlaceUser', {
-    'id': fields.String(description='User ID'),
-    'first_name': fields.String(description='First name of the owner'),
-    'last_name': fields.String(description='Last name of the owner'),
-    'email': fields.String(description='Email of the owner')
-})
+    def __init__(self, title, description, price, latitude, longitude, owner, amenities):
+        if not isinstance(price, (int, float)):
+            raise TypeError("Price must be a number")
+        if not isinstance(title, str):
+            raise TypeError("Title must be a string")
+        if not isinstance(latitude, (int, float)):
+            raise TypeError("Latitude must be a number")
+        if not isinstance(longitude, (int, float)):
+            raise TypeError("Longitude must be a number")
+        super().__init__()
+        self.title = title
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner = owner
+        self.reviews = []
+        self.amenities = amenities
 
-place_model = api.model('Place', {
-    'title': fields.String(required=True, description='Title of the place'),
-    'description': fields.String(description='Description of the place'),
-    'price': fields.Float(required=True, description='Price per night'),
-    'latitude': fields.Float(required=True, description='Latitude of the place'),
-    'longitude': fields.Float(required=True, description='Longitude of the place'),
-    'owner_id': fields.String(required=True, description='ID of the owner'),
-    'amenities': fields.List(fields.String, required=True, description="List of amenities ID's")
-})
-
-@api.route('/')
-class PlaceList(Resource):
-    @api.expect(place_model)
-    @api.response(201, 'Place successfully created')
-    @api.response(400, 'Invalid input data')
-    def post(self):
-        """Register a new place"""
-        pass
-
-    @api.response(200, 'List of places retrieved successfully')
-    def get(self):
-        """Retrieve a list of all places"""
-        pass
-
-@api.route('/<place_id>')
-class PlaceResource(Resource):
-    @api.response(200, 'Place details retrieved successfully')
-    @api.response(404, 'Place not found')
-    def get(self, place_id):
-        """Get place details by ID"""
-        pass
-
-    @api.expect(place_model)
-    @api.response(200, 'Place updated successfully')
-    @api.response(404, 'Place not found')
-    @api.response(400, 'Invalid input data')
-    def put(self, place_id):
-        """Update a place's information"""
-        pass
+    @property
+    def title(self):
+        return self._title
--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,29 +1,69 @@
 #!/usr/bin/python3
 
 
-from flask_restx import Namespace, Resource, fields
-from app.services import facade
+from .basemodel import BaseModel
+import re
 
-api = Namespace('users', description='User operations')
-user_model = api.model('User', {
-    'first_name': fields.String(required=True, description='First name of the user'),
-    'last_name': fields.String(required=True, description='Last name of the user'),
-    'email': fields.String(required=True, description='Email of the user')
-})
+def validate_email(email):
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z{2,}$'
+    if re.match(pattern, email):
+        return email
+    raise ValueError("Error format email")
 
-@api.route('/')
-class UserList(Resource):
-    @api.expect(user_model, validate=True)
-    @api.response(201, 'User successfully created')
-    @api.response(400, 'Email already registered')
-    @api.response(400, 'Invalid input data')
-    def post(self):
-        """Register a new user"""
-        user_data = api.payload
+def validate_name(name):
+    if not isinstance(name, str):
+        raise ValueError("Name must be a string")
+    return name
 
-        existing_user = facade.get_user_by_email(user_data['email'])
-        if existing_user:
-            return {'error': 'Email already registered'}, 400
+class User(BaseModel):
 
-        new_user = facade.create_user(user_data)
-        return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
+    def __init__(self, first_name=None, last_name=None, email=None, isadmin=False):
+        if first_name = None:
+            raise ValueError("first name requiered")
+        if last_name = None:
+            raise ValueError("last name requiered")
+        if email = None:
+            raise ValueError("email requiered")
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.isadmin = isadmin
+
+    @property
+    def is_admin(self):
+        return self.__is_admin
+
+    @is_admin.setter
+    def is_admin(self, value):
+        if isinstance(value, bool):
+            self__is_damin = value #verify is_admin is False or True
+        else:
+            raise ValueError("isadmin must be True or False")
+    
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, value):
+        if validate_email(value) == value:
+            self.__email = value
+
+    @property
+    def first_name(self):
+        return self.first_name
+
+    @first_name.setter
+    def first_name(self, value):
+        if validate_name == value:
+            self.__first_name = value
+
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    @last_name.setter
+    def last_name(self, value):
+        if validate_name == value:
+            self.__last_name = value
